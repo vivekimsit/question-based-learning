fa525692ac92f9a84b06c24b6dcdeaa003f535a8
Vivek Poddar <vivekimsit@gmail.com>
2017-03-26 15:51:21 +0530

    Font Face Oberver.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
+import FontFaceObserver from 'fontfaceobserver';
 import configureStore from './configureStore';
 
 const store = configureStore();
+
+const roboto400 = new FontFaceObserver("Roboto", {
+  weight: 400
+});
+const roboto700 = new FontFaceObserver("Roboto", {
+  weight: 500
+});
+
+Promise.all([
+  roboto400.load(),
+  roboto700.load()
+]).then(function() {
+  document.documentElement.className += " fonts-loaded";
+});
 
 render(
   <Root store={store} />,
